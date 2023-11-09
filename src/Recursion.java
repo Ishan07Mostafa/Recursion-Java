6b5492561eb227c35243b3927228913c322658f8
Ishan07Mostafa <ishan.mostafa7@gmail.com>
2023-11-10 01:13:39 +0600

    third commit

--- src/Recursion.java
+++ src/Recursion.java
@@ -20,19 +20,19 @@ public class Recursion {
     }
 
     //Implementing n-factorial method using recursion
-    public static int nFactorial(int n){
+    private static int nFactorial(int n){
         if(n==0 || n==1) return n;
         else return n*nFactorial(n-1);
     }
 
     //Implementing Fibonacci series using recursion
-    public static int fibonacci(int n){
+    private static int fibonacci(int n){
         if(n==0 || n==1) return n;
         else return fibonacci(n-1)+fibonacci(n-2);
     }
 
     //Printing an array of integers using recursion
-    public static void arrayPrint(int[] array, int n){
+    private static void arrayPrint(int[] array, int n){
         if(n>0){
             arrayPrint(array, n-1);
             if(n-1==array.length-1) System.out.print(array[n-1]+".");
@@ -41,13 +41,13 @@ public class Recursion {
     }
 
     //Summation of all integers in an array using recursion
-    public static int arraySum(int[] array, int n){
+    private static int arraySum(int[] array, int n){
         if(n==0) return array[n];
         else return array[n] + arraySum(array,n-1);
     }
 
     //Finding exponents using recursion
-    public static int exponent(int m, int n){
+    private static int exponent(int m, int n){
         if(n==0) return 1;
         else return m*exponent(m,n-1);
     }
@@ -56,7 +56,7 @@ public class Recursion {
     Implementing a method where a decimal number is converted
     to binary with the help of recursion
     */
-    public static String dec2bin(int n){
+    private static String dec2bin(int n){
         int rem;
         if(n==1) return "1";
         else{
